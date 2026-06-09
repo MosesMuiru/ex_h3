d94b6d80db8a5af3e0b8230ca9cda0ec9b3df2c1
moses <muirumoses9@gmail.com>
2026-06-10 02:05:37 +0300

    cell boundary

--- c_src/h3.c
+++ c_src/h3.c
@@ -1,12 +1,39 @@
 #include <h3/h3api.h>
 #include <erl_nif.h>
 
+static ERL_NIF_TERM make_list_from_array(ErlNifEnv *env, CellBoundary *gp) {
+    ERL_NIF_TERM list = enif_make_list(env, 0);
+
+    for(int i = gp->numVerts - 1; i >= 0; i--) {
+        ERL_NIF_TERM lat = enif_make_double(env, radsToDegs(gp->verts[i].lat));
+        ERL_NIF_TERM lng = enif_make_double(env, radsToDegs(gp->verts[i].lng));
+
+        ERL_NIF_TERM point = enif_make_tuple2(env, lat, lng);
+
+        list = enif_make_list_cell(env, point, list);
+    }
+
+    
+    return list;
+};
+
+// check what is contained under erlnifenv
+static ERL_NIF_TERM make_h3_error(ErlNifEnv* env, H3Error err)
+{
+    return enif_make_tuple2(env, enif_make_atom(env, "error"), err);
+}
+
+// used to return tuple with {:ok, {data}}
+static ERL_NIF_TERM make_h3_success_return(ErlNifEnv* env, ERL_NIF_TERM term){
+    return enif_make_tuple2(env, enif_make_atom(env, "ok"), term);
+}
 
 static ERL_NIF_TERM
 make_h3idx(ErlNifEnv * env, H3Index index)
 {
     return enif_make_uint64(env, index);
 }
+
 
 static ERL_NIF_TERM 
 enif_make_lat_lng(ErlNifEnv *env, LatLng *g)
@@ -42,40 +69,57 @@ static ERL_NIF_TERM lat_lng_to_cell(ErlNifEnv* env, int argc, const ERL_NIF_TERM
         return enif_make_badarg(env);
 
     /* call H3 */
+    latLng.lat = degsToRads(latLng.lat);
+    latLng.lng = degsToRads(latLng.lng);
     H3Error err = latLngToCell(&latLng, res, &out);
 	H3Error er = h3ToString(out, buf, sizeof(buf));
 
 
     /* return result */
     // return enif_make_uint64(env, out);
-    return enif_make_string(env, buf, ERL_NIF_UTF8);
+    return make_h3_success_return(env, enif_make_string(env, buf, ERL_NIF_UTF8));
 }
 
 static ERL_NIF_TERM cell_to_lat_lng(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]){
 	H3Index h3;
+    H3Error err;
 	LatLng g;
 	char buf[17];
 
     if(!enif_get_string(env, argv[0], buf, sizeof(buf), ERL_NIF_UTF8))
         return enif_make_badarg(env);
 
-	
-    	H3Error err = cellToLatLng(h3, &g);
-	
-	return enif_make_tuple2(env, enif_make_double(env, g.lat),  enif_make_double(env, g.lng));
+    err = stringToH3(buf, &h3);
+    if(err != E_SUCCESS)
+        return make_h3_error(env, err);
 
+    err = cellToLatLng(h3, &g);
+    if(err != E_SUCCESS)
+        return make_h3_error(env, err);
+    g.lat = radsToDegs(g.lat);
+    g.lng = radsToDegs(g.lng);
+
+    return make_h3_success_return(env,  enif_make_tuple2(env, enif_make_double(env, g.lat),  enif_make_double(env, g.lng)));
 }
 
 static ERL_NIF_TERM cell_to_boundary(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]){
 	H3Index h3;
+    H3Error err;
 	CellBoundary gp;
     char buf[17];
 
     if(!enif_get_string(env, argv[0], buf, sizeof(buf), ERL_NIF_UTF8))
         return enif_make_badarg(env);
-	
-	H3Error err = cellToBoundary(h3, &gp);
-	return enif_make_tuple2(env, enif_make_int(env, gp.numVerts), enif_make_lat_lng(env, gp.verts));
+    
+    err = stringToH3(buf, &h3);
+    if(err != E_SUCCESS)
+        return make_h3_error(env, err);
+
+    err = cellToBoundary(h3, &gp);
+    if(err != E_SUCCESS)
+        return make_h3_error(env, err);
+
+	return enif_make_tuple2(env, enif_make_int(env, gp.numVerts), make_list_from_array(env, &gp));
 }
 
 
