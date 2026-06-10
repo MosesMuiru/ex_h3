949104d9ac717a7b3e07999682bacbef207fd04e
moses <muirumoses9@gmail.com>
2026-06-10 17:24:34 +0300

    fix formatting

--- lib/ex_h3.ex
+++ lib/ex_h3.ex
@@ -10,9 +10,9 @@ defmodule ExH3 do
   # h3_index is 64
   @type h3_index :: non_neg_integer()
   @type boundary_t :: %{
-    num_verts: non_neg_integer(),
-    verts: list(lat_lng_t())
-  }
+          num_verts: non_neg_integer(),
+          verts: list(lat_lng_t())
+        }
 
   def lat_lng_to_cell({_lat, _lng}, _res), do: :erlang.nif_error(:nif_not_loaded)
 
@@ -29,7 +29,8 @@ defmodule ExH3 do
   @spec grid_disk(h3_index(), non_neg_integer()) :: list(h3_index())
   def grid_disk(_origin, _k), do: :erlang.nif_error(:nif_not_loaded)
 
-  @spec grid_disk_distances(h3_index(), non_neg_integer()) :: list({h3_index(), non_neg_integer()})
+  @spec grid_disk_distances(h3_index(), non_neg_integer()) ::
+          list({h3_index(), non_neg_integer()})
   def grid_disk_distances(_origin, _k), do: :erlang.nif_error(:nif_not_loaded)
 
   @spec max_grid_ring_size(non_neg_integer()) :: non_neg_integer()
--- test/ex_h3_test.exs
+++ test/ex_h3_test.exs
@@ -3,14 +3,14 @@ defmodule ExH3Test do
   doctest ExH3
 
   setup_all do
-  lat_lng = {37.3615593, -122.0553238}
-  res = 10
-  state = %{lat_lng: lat_lng, res: res, expected_index: ~c"8a283470d927fff"}
-  {:ok, state}
+    lat_lng = {37.3615593, -122.0553238}
+    res = 10
+    state = %{lat_lng: lat_lng, res: res, expected_index: ~c"8a283470d927fff"}
+    {:ok, state}
   end
 
   test "converts lat/lng to H3 index", state do
-    {:ok, index} =  ExH3.lat_lng_to_cell(state[:lat_lng], state[:res])
+    {:ok, index} = ExH3.lat_lng_to_cell(state[:lat_lng], state[:res])
     assert index == state[:expected_index]
   end
 
@@ -18,25 +18,25 @@ defmodule ExH3Test do
     {:ok, {lat, lng}} = ExH3.cell_to_lat_lng(state[:expected_index])
     lat = trunc(lat)
     lng = trunc(lng)
-    assert lat  == state[:lat_lng] |> elem(0) |> trunc()
+    assert lat == state[:lat_lng] |> elem(0) |> trunc()
     assert lng == state[:lat_lng] |> elem(1) |> trunc()
   end
 
   test "converts cell to boundary", state do
-    {expected_num_vertices, expected_vertices} =  {6,
-      [
-        {37.36081881948407, -122.05466732876516},
-        {37.361324418946275, -122.05402753508244},
-        {37.361998502992506, -122.054231694951},
-        {37.36216698379974, -122.055075655472},
-        {37.36166138188755, -122.05571544499882},
-        {37.36098730161813, -122.05551127816072}
-      ]}
+    {expected_num_vertices, expected_vertices} =
+      {6,
+       [
+         {37.36081881948407, -122.05466732876516},
+         {37.361324418946275, -122.05402753508244},
+         {37.361998502992506, -122.054231694951},
+         {37.36216698379974, -122.055075655472},
+         {37.36166138188755, -122.05571544499882},
+         {37.36098730161813, -122.05551127816072}
+       ]}
+
     {:ok, {num_vertices, vertices}} = ExH3.cell_to_boundary(state[:expected_index])
 
-
-    assert expected_num_vertices == num_vertices 
+    assert expected_num_vertices == num_vertices
     assert expected_vertices == vertices
-
   end
 end
